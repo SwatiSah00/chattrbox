78274bbe4e92d19dec236ddd982eb32e414646f9
SwatiSah00 <swatisahoo.nist@gmail.com>
2018-03-21 15:55:12 -0700

    homework 5 submission2

--- websockets-server.js
+++ websockets-server.js
@@ -5,8 +5,40 @@ var ws = new WebSocketServer({
   port: port
 });
 var messages = [];
+var currentTopic = '';
 console.log('websockets server started');
 ws.on('connection', function(socket) {
+  console.log('client connection established');
+  if(currentTopic == ''){
+    messages.forEach(function(msg) {
+      socket.send(msg);
+    });
+
+  } else {
+    socket.send("*** Topic is '" + currentTopic + "'");
+    messages.forEach(function(msg) {
+      socket.send(msg);
+    });
+  }
+  socket.on('message', function(data) {
+    console.log('message received: ' + data);
+    var substring = '/topic';
+    if(data.indexOf(substring) == 0){
+      currentTopic = data.slice(6);
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send("*** Topic has changed to '" + currentTopic + "'")
+      });
+    } else {
+      messages.push(data);
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send(data)
+      });
+
+    }
+
+  });
+});
+/*ws.on('connection', function(socket) {
   console.log('client connection established');
   messages.forEach(function(msg) {
     socket.send(msg);
@@ -18,7 +50,8 @@ ws.on('connection', function(socket) {
       clientSocket.send(data)
     });
   });
-});
+});*/
+
 /*ws.on('connection', function(socket) {
   console.log('client connection established');
   messages.forEach(function(msg) {
